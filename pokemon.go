d51089012372c75aa4d99a8703c2ef85a010e35f
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-09-10 13:03:56 +0800

    getName implemented

--- pokemon.go
+++ pokemon.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+	"os"
+)
+
+
 func getName(id int, lang string) string{
-	return "foo"
+	file, e := ioutil.ReadFile("./data/" + lang + ".json")
+	if e != nil {
+        fmt.Printf("File error: %v\n", e)
+        os.Exit(1)
+    }
+    var pokemons []string
+    json.Unmarshal(file, &pokemons)
+
+	return pokemons[id - 1]
 }
--- pokemon_test.go
+++ pokemon_test.go
@@ -10,6 +10,9 @@ func TestEnNames(t *testing.T) {
 		{1, "en", "Bulbasaur"},
 		{400, "en", "Bibarel"},
 		{721, "en", "Volcanion"},
+		{1, "zh-Hant", "妙蛙種子"},
+		{400, "zh-Hant", "大尾狸"},
+		{721, "zh-Hant", "波爾凱尼恩"},
 	}
 	for _, c := range cases {
 		got := getName(c.id, c.lang)
